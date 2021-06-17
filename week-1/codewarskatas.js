6b0f5d2be08bcb1bf2a3e255235851df91446c49
cerealenjoyer <sasha.antilyons@gmail.com>
2021-06-17 16:59:19 +0100

    updated js file

---
+++ week-1/codewarskatas.js
@@ -0,0 +1,12 @@
+//Solution for 8kyu problem Sum the Strings (pair programming)
+function sumStr(a,b) {
+    return ((parseInt(a)|0)+(parseInt(b)|0)).toString()
+  }
+//Solution for 8kyu problem Will you make it? (pair programming)
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => 
+(mpg*fuelLeft >= distanceToPump );
+
+// Solution for 7kyu problem Over The Road
+function overTheRoad(address, n) {
+    return (2*n + 1) - address
+   }
