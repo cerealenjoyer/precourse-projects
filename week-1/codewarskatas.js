d10b8339b2b67cb379a063dbb7588226483fe3fd
cerealenjoyer <sasha.antilyons@gmail.com>
2021-06-18 15:40:54 +0100

    added challenges from Thursdays workshop

--- week-1/codewarskatas.js
+++ week-1/codewarskatas.js
@@ -10,3 +10,23 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
 function overTheRoad(address, n) {
     return (2*n + 1) - address
    }
+
+//Mumbling Solution
+function accum(s) {
+  const array = s.toLowerCase().split("");
+  const stretch = array.map( (a,index) => a.repeat(index+1)); 
+  const caps = stretch.map(a => a[0].toUpperCase() + a.slice(1));
+return caps.join('-')
+}
+
+//Descending Order Solution
+const descendingOrder = (n) => parseInt(n.toString().split('').sort().reverse().join(''))
+
+//Highest and Lowest Solution
+function highAndLow(numbers){
+const array1 = numbers.split(' ');
+return `${Math.max(...array1)} ${Math.min(...array1)}`;
+}
+
+//You're a square Solution
+const isSquare =(n) => Number.isInteger(Math.sqrt(n))
