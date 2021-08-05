bf8156c1845f634e1055fc6860ac63fca63337f7
cerealenjoyer <sasha.antilyons@gmail.com>
2021-08-05 12:14:20 +0100

    added pokemon lookup project

---
+++ week-8/real-world-fetch/scripts.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("form");
+const output = document.querySelector("output");
+const mimeCheck = (pokemon) => pokemon.split("-").join(" ");
+const unMime = (pokemon) => pokemon.split(" ").join("-");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const dataLookup = new FormData(form);
+  const name = unMime(dataLookup.get("name"));
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+    .then((response) => {
+      output.innerHTML = "";
+      if (!response.ok) {
+        const error = new Error(response.status);
+        throw error;
+      } else {
+        return response.json();
+      }
+    })
+
+    .then((pokemon) => {
+      console.log(pokemon);
+      const pokeName = document.createElement("h2");
+
+      const checkName = mimeCheck(name);
+      pokeName.innerText =
+        checkName.charAt(0).toUpperCase() + checkName.slice(1) + "!";
+
+      output.append(pokeName);
+      const pokeImage = document.createElement("img");
+      console.log(pokemon.sprites.other);
+      pokeImage.src = Object.values(
+        pokemon.sprites.other["official-artwork"]
+      )[0];
+      output.append(pokeImage);
+      form.reset();
+    })
+    .catch((error) => {
+      if (error.message === "404") {
+        output.textContent =
+          "That's not a pokemon! Did you spell " + name + " right?";
+      } else output.textContent = "something else went wrong idk";
+    });
+});
